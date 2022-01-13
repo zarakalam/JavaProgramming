5df29aac0c3b1631a9cd19bcab3b29495984a592
Zarak A <alamz523@gmail.com>
2022-01-13 09:54:38 -0600

    day38: added Toyota class

---
+++ src/day38_InheritanceIII/CarTask/Toyota.java
@@ -0,0 +1,15 @@
+package day38_InheritanceIII.CarTask;
+
+public class Toyota extends Car{
+
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles){
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand+" "+model+" is reliable");
+    }
+
+
+}
