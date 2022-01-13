81da7810a4a5d58232bd7df9f869d9144b331ce8
Zarak A <alamz523@gmail.com>
2022-01-13 10:08:04 -0600

    day38: test case for Car complete

---
+++ src/day38_InheritanceIII/CarTask/CarObjective.java
@@ -0,0 +1,14 @@
+package day38_InheritanceIII.CarTask;
+
+public class CarObjective {
+    public static void main(String[] args) {
+
+        BMW bmw1 = new BMW("BMW", "325", 2021, 34995, "black", 25);
+
+        System.out.println(bmw1);
+        bmw1.racing();
+
+
+
+    }
+}
