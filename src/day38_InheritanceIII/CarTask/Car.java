c278149b2e55c3cfe398c13aaa46e2ade4e7d935
Zarak A <alamz523@gmail.com>
2022-01-13 09:38:07 -0600

    day38: Inheritance Continue - Method Overriding

--- src/day38_InheritanceIII/CarTask/Car.java
+++ src/day38_InheritanceIII/CarTask/Car.java
@@ -1,4 +1,46 @@
 package day38_InheritanceIII.CarTask;
 
 public class Car {
+
+    public String brand;
+    public String model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+
+    public Car(String brand, String model, int year, double price, String color, int miles){
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+        this.price=price;
+        this.color=color;
+        this.miles=miles;
+    }
+
+
+    public void start(){
+        System.out.println(brand+" "+model+ " is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand+" "+model+ " is driving");
+    }
+
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+
+
+
+
 }
