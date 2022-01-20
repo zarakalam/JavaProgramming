12c48a8040b13126f6b60f8c088445770b3515dc
Zarak A <alamz523@gmail.com>
2022-01-20 10:03:26 -0600

    JAVA programming

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,8 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo"; // default access modifier
+
+    protected static String name2 = "woodenSpoon"; // protected access modifier
+}
---
+++ src/utilities/AccessModifiers_Test3.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiers_Test3 {
+
+    public static void main(String[] args) {
+
+        // System.out.println(ProtectedAccessModifier.name1);  //default is not visible outside package
+        // System.out.println(ProtectedAccessModifier.name2); // protected not always visible outside package... needs to be subclass aka add extends to Class
+
+
+
+
+
+
+    }
+}
