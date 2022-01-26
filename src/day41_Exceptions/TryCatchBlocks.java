6b38f593e3f829e0b8dbd43b20efc275cfdad66a
Zarak A <alamz523@gmail.com>
2022-01-26 16:51:35 -0600

    Day 41 Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
@@ -51,21 +54,31 @@ public class TryCatchBlocks {
 
         System.out.println("Test 3 started");
 
-        try {
 
-            System.out.println("Cydeo".substring(2, 0));
+        System.out.println("Test 3 Completed");
 
-            System.out.println("try block");
+        System.out.println("=================================================");
 
-        }catch(RuntimeException e){
+        System.out.println("Hello");
 
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
             e.printStackTrace();
+        }
 
+        System.out.println("Cydeo");
 
+        System.out.println("=========================");
+
+        try {
+            FileInputStream file = new FileInputStream("File Path");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
         }
 
 
-        System.out.println("Test 3 Completed");
+
 
 
 
