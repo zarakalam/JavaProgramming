a0a5c17c18d852ad9a0f580811380d15c923087d
Zarak A <alamz523@gmail.com>
2022-01-27 09:55:03 -0600

    day 42 JAVA Programming

---
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,35 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("----------Test 1--------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);  // now theres no error, and dont have to add try and catch block
+
+        System.out.println("Cydeo");
+
+
+
+        System.out.println("----------Test 1 completed--------------");
+
+
+
+        System.out.println("----------Test 2--------------");
+
+        System.out.println("Hello");
+
+        Thread.sleep(3000);
+
+        System.out.println("Batch 25");
+
+        System.out.println("----------Test 2 completed--------------");
+
+
+
+    }
+}
