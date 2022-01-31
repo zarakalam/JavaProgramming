223ecb4ce17a23da7140d6129d1c7d4709890bf0
Zarak A <alamz523@gmail.com>
2022-01-31 13:38:18 -0600

    day 43 Abstraction

--- src/day43_Abstraction/Car/Car.java
+++ src/day43_Abstraction/Car/Car.java
@@ -52,7 +52,9 @@ public abstract class Car {
     }
 
 
-    public abstract void stop();
+    public void stop(){
+        System.out.println("Press the brake");
+    }
 
     public abstract void start();
 
--- src/day43_Abstraction/Car/Honda.java
+++ src/day43_Abstraction/Car/Honda.java
@@ -1,8 +1,8 @@
 package day43_Abstraction.Car;
 
 public class Honda extends Car {
-    public Honda(String brand, String model, String color, int year, double price) {
-        super(brand, model, color, year, price);
+    public Honda(String model, String color, int year, double price) {
+        super("Honda", model, color, year, price);
     }
 
     public void stop(){
