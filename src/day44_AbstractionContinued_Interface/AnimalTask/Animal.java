3efe45273f56eac6fbbd8a1bbcef9910997a555f
Zarak A <alamz523@gmail.com>
2022-02-03 10:55:14 -0600

    day 44 Abstraction

--- src/day44_AbstractionContinued_Interface/AnimalTask/Animal.java
+++ src/day44_AbstractionContinued_Interface/AnimalTask/Animal.java
@@ -1,6 +1,6 @@
 package day44_AbstractionContinued_Interface.AnimalTask;
 
-public class Animal {
+public abstract class Animal {
 
     private String name;
     private final String breed;
@@ -9,6 +9,76 @@ public class Animal {
     private String size;
     private final String color;
 
+    public static boolean canBreathe = true;
+
+    static{
+        canBreathe = true;
+    }
 
 
+    public Animal(String name, String breed, char gender, int age, String size, String color) {
+        setName(name);
+        this.breed = breed;
+        if( !(gender=='M' || gender=='F') ){
+            throw new RuntimeException("Invalid gender: "+gender);
+        }
+        this.gender = gender;
+        setAge(age);
+        setSize(size);
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+    }
+
+    public abstract void eat();
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
