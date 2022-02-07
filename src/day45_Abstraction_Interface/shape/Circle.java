d68ddfbe55a2ae0a3d62a31f1bef76488833f7e3
Zarak A <alamz523@gmail.com>
2022-02-07 14:06:16 -0600

    day 45 Interface

--- src/day45_Abstraction_Interface/shape/Circle.java
+++ src/day45_Abstraction_Interface/shape/Circle.java
@@ -1,4 +1,32 @@
 package day45_Abstraction_Interface.shape;
 
-public class Circle {
+public class Circle extends Shape{
+
+    public Circle(double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    private double radius;
+
+    public final static double pi= 3.14;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius < 0){
+            throw new RuntimeException("invalid response");
+        }
+        this.radius = radius;
+    }
+
+    public double area(){
+        return radius*radius*pi;
+    }
+
+    public double perimeter(){
+        return 2*radius*pi;
+    }
 }
