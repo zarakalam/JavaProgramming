858c2e01db63cbb09a2a18644ab9a2d79b972e78
Zarak A <alamz523@gmail.com>
2022-02-14 11:49:23 -0600

    day 47 polymorphism

--- src/day47_Polymorphism/ReferenceTypeCastings.java
+++ src/day47_Polymorphism/ReferenceTypeCastings.java
@@ -1,9 +1,13 @@
 package day47_Polymorphism;
 
+import day39_Recap.cydeoTask.Employee;
+import day39_Recap.cydeoTask.Tester;
 import day44_AbstractionContinued_Interface.AnimalTask.Animal;
+import day44_AbstractionContinued_Interface.AnimalTask.Cat;
 import day44_AbstractionContinued_Interface.AnimalTask.Dog;
 import day45_Abstraction_Interface.shape.Circle;
 import day45_Abstraction_Interface.shape.Shape;
+import day45_Abstraction_Interface.shape.Square;
 
 public class ReferenceTypeCastings {
 
@@ -21,6 +25,26 @@ public class ReferenceTypeCastings {
         System.out.println(dog.getName());
 
         dog.bark();
+
+        Shape shape1 = new Square(5);
+
+        System.out.println( ((Square) shape1).getSide() );
+        System.out.println("=========================================");
+
+        Animal animal2 = new Cat("Jim", "Scottish", 'M', 3, "Small", "White");
+
+       //Cat cat = (Cat) animal2;
+       //cat.meow();
+
+        ((Cat) animal2).meow();
+        //((Dog) animal2 ).bark(); // will get compile error because animal2 is related to Cat attributes and cat doesnt bark
+        System.out.println("=======================================");
+
+        Employee employee = new Tester("Ali", 30, 'M', 2232, "Tester", 100000);
+
+        ((Tester)employee).work();
+        ((Tester)employee).createTicket();
+
     }
 
 }
